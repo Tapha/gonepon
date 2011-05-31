de19788052038c03435343285818e33a0f7ec909
Tapha Ngum <tapha@live.co.uk>
2011-05-31 19:54:03 +0300

    add setup

---
+++ application/views/home.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>gonepon - "The Exclusive Local Deal"</title>
+	<link rel="icon" type="image/png" href="<?php echo $base_url."img/favicon.ico";?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo $base_url.'css/style.css';?>" />
+</head>
+<body>
+<div id='header'>
+	<div id='logo'>
+		<img src="<?php echo $base_url."img/logo.png";?>">
+	</div>
+</div>
+</body>
+</html>
