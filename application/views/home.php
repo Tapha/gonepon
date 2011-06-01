a02e7d3be708f64f6ce076e7f50f9cbab42b3c8a
Tapha Ngum <tapha@live.co.uk>
2011-06-01 16:28:07 +0300

    ma landing page

--- application/views/home.php
+++ application/views/home.php
@@ -1,16 +1,203 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<title>gonepon - "The Exclusive Local Deal"</title>
-	<link rel="icon" type="image/png" href="<?php echo $base_url."img/favicon.ico";?>">
-	<link rel="stylesheet" type="text/css" href="<?php echo $base_url.'css/style.css';?>" />
+<html><head>
+
+
+<title>GonePon - The Limited Coupon | Up to 99% Discounts in Central London</title>
+<meta name="description" content="Job Search by Indeed. one search. all uk jobs. Search millions of jobs from thousands of job boards, newspapers, classifieds and company websites on indeed.co.uk">
+<meta name="keywords" content="job search, Indeed, jobs, search engine for jobs, job search engine, job listings, search jobs, career, employment, work, find jobs, rss jobs feed xml british">
+
+
+<link rel="canonical" href="/">
+<link rel="shortcut icon" href="/favicon.ico">
+<link rel="alternate" media="handheld" href="/m/">
+<link rel="search" type="application/opensearchdescription+xml" title="Indeed Job Search" href="/indeedsearchdescription.xml">
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+<style type="text/css">
+body { background:#fff; margin:2px 5px }
+body,td { font-family: Arial,sans-serif; font-size:10pt }
+#jobsearch { margin: 4px 0 0; }
+#jobsearch label { font-size:14pt }
+a:link { color:#00c }
+a:visited { color:#551a8b }
+a:active { color:#f00 }
+.sl,
+.sl:link,
+.sl:visited { color: #77c }
+.searches { table-layout:fixed; overflow: hidden; margin-top: 4px }
+.trunc { font-size:smaller }
+.nji { color: #f60; white-space: nowrap; }
+.searches td { padding:3px 1px }
+.rsc { text-align:right; white-space:nowrap; color:#aaa }
+#populartable { border-bottom: 1px solid #f60 }
+#rsh { padding: 0 0 4px; padding-right:2em; border-bottom: 1px solid #EBEBEB; color: #6F6F6F; }
+#rfh { border-bottom: 1px solid #f60 }
+.hr { color: #ccc }
+.footer a:visited{color:#00c}
+.footer .sl:visited{color: #77c}
+#countries a{white-space:nowrap;font-size:9pt;}
+
+.acd { border: 1px solid #333; background: #fff; position:absolute; width:100%; z-index: 1; }
+.aci { font-size: 18px; padding:1px 6px; cursor:pointer; }
+.acis { background:#36c; color:#fff; }
+#acr td .h { display:none; }
+#acr td { padding-top:0; padding-bottom:0; }
+
+.newmsg { color: #F03 }
+
+.inwrap { border-right: 1px solid #e8e8e8;border-bottom: 1px solid #e8e8e8; display:inline-block; width:100%; }
+.inwrap input { margin:0; height: 30px;  font-family:Arial,sans-serif;border:1px solid #ccc;border-bottom-color:#aaa;border-right-color:#aaa; -webkit-border-radius: 0; -webkit-appearance: none; }
+.inwrap .input_text {width: 100%; font-size:18px;padding:3px 6px;_margin: -1px 0;}
+.inwrap .input_submit {font-size:15px;height:30px;background: #e8e8e8; padding:3px 9px;cursor:pointer;_padding:3px;}
+.inwrap .input_submit:active { background: #ccc; }
+.npb { padding-bottom: 0; color: #f60; text-transform: lowercase;font-weight:bold; }
+.npl { padding-left: 0 }
+
+#jobsearch .label {font-size:12px; line-height:1.2;padding-top:0;color:#aaa;font-weight:normal;white-space:nowrap;padding-right:1.5em}
+#jobsearch .sl { font-size: 11px; color: #77c; white-space: nowrap;  }
+
+
+
+</style>
+
+
+
+
 </head>
 <body>
-<div id='header'>
-	<div id='logo'>
-		<img src="<?php echo $base_url."img/logo.png";?>">
-	</div>
+
+
+
+
+
+
+
+
+
+
+<style type="text/css">#g_nav { border-bottom:1px solid #ccc; padding: 0 0 2px; margin-bottom: 9px; }</style>
+
+
+<div>
+
+<table width="100%">
+<tbody><tr>
+<td style="padding-left:0.5em" nowrap="">&nbsp;</td>
+<td style="padding-right:0.5em" align="right" nowrap="">
+
+
+<style type="text/css">
+
+#u_nav a,
+#u_nav a:visited { color: #00c; text-transform: lowercase; }
+#navpromo a,
+#navpromo a:visited {color: #F03; text-transform: none; }
+#u_nav .login_unconfirmed, #u_nav .login_unconfirmed a, #u_nav .login_unconfirmed a:visited { color: #F03 }
+#u_nav .resume_pending, #u_nav .resume_pending a, #u_nav .resume_pending a:visited { color: #F03 }
+</style>
+<div id="u_nav">
+ 
+
+
+
+
+
+<div id="user_actions">
+
+
+
+
+
+<a href="" rel="nofollow">sign up</a> -
+
+
+
+
+
+<a href="" rel="nofollow">sign in</a>
+
+
+
+
+ -
+<a href="">FAQ</a>
 </div>
-</body>
-</html>
+</div>
+</td>
+</tr>
+</tbody></table>
+</div>
+
+
+
+
+
+
+<table width="700" border="0" cellspacing="0" cellpadding="0" align="center" style="margin-top:1em">
+<tbody><tr>
+<td><img border="0"  src="<?php echo $base_url.'img/logo.png'; ?>" alt="" "=""gonepon" title="Literally the best deals you will find">
+</td></tr>
+<tr>
+<td>
+<form action="/jobs" method="get" name="jobsearch" id="jobsearch" onsubmit="formptk('form','where_ac');">
+<table cellspacing="0" cellpadding="3" width="100%" align="center">
+<tbody><tr>
+<td class="npb"><label for="what">what</label></td>
+<td class="npb" colspan="2"><label for="where">where</label></td>
+</tr>
+<tr>
+<td class="npl epr"><span class="inwrap"><input class="input_text" maxlength="512" size="31" id="what" name="q"></span><div style="width:250px"><!-- --></div></td>
+<td class="npl epr"><span class="inwrap"><input class="input_text" maxlength="64" size="27" id="where" name="l" autocomplete="off"></span><div style="width:200px"><!-- --></div></td>
+<td class="npl" style="width:1px"><span class="inwrap" style="width:auto;"><input id="fj" type="submit" class="input_submit" value="Find gonepons!"></span></td>
+</tr>
+
+<tr id="acr">
+<td><span class="h">&nbsp;</span></td>
+<td colspan="2" class="npl"><div style="position:relative;z-index:2"><div id="acdiv" class="acd" style="display:none;"></div></div></td>
+</tr>
+
+<tr valign="baseline">
+<td class="label">title, keywords or item name</td>
+<td class="label">city or postcode</td>
+<td style="padding-top:0"><a class="sl" href="/">Advanced Search</a></td>
+</tr>
+</tbody></table>
+</form>
+<br>
+<div style="text-align:center;margin:1.25em 0 0.4em">
+
+125  <a href="/">gonepons</a> left in Central London<br>
+
+<p>
+Up to 99% off restaurants, spas and all other types of experiences in Central London!
+</p>
+<p>
+But there are only a limited number of coupons posted per day (about 35 per location).
+</p>
+<p>
+So you need to be quick! Search gonepon to get yours now!
+</p>
+</div>
+<hr class="hr" size="1">
+<br>
+<div align="center" class="footer" style="clear:both">
+    <span style="white-space:nowrap">
+    
+    
+    
+    
+    <a href="/">Browse Gonepons</a> - 
+    
+     <a href=""><nobr>About</nobr></a> -
+    
+    <a href="">Get your business on gonepon!</a> -
+    
+    </span>
+    
+    
+    <div style="margin-top:1em"><font size="-2">©2011 Gonepon</font></div>
+</div>
+<br>
+
+</td>
+</tr>
+</tbody></table>
+</body></html>
